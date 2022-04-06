7510396f7b5b781b2e6ea6a839067b69528045a5
yds <yds@ichub.com>
2022-04-06 11:12:30 +0800

    添加备注

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import reactJsx from 'vite-react-jsx'
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // react17不需要在每个文件中引用react，但是vite不支持，使用插件改造
     reactJsx(),
   ],
   resolve: {
